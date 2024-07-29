a8066352a5754ab3b942e30149ffff8376043bed
mayur4279 <mayurdhotre.a15@gmail.com>
2024-07-30 00:23:21 +0530

    first terrafrom apply

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_vpc" "main" {
 }  
 
 #basically we are created VPC with lot of range.  
-#withing the vpc range you are taking the small area and creating a small range called subnet.   
+#withing that vpc range you are taking the small area and creating a small range called subnet.   
 
 
 
@@ -27,6 +27,7 @@ resource  "aws_subnet" "sub-2"  {                 # subnet 2
 }
 
 
+
 resource "aws_internet_gateway" "igw" {          #internet gateway 
   vpc_id = aws_vpc.main.id
   tags = {
@@ -35,28 +36,26 @@ resource "aws_internet_gateway" "igw" {          #internet gateway
 } 
 
 
-resource "aws_route_table" "rt" {                     #route table  
-  vpc_id = aws_vpc.main.id     
+# resource "aws_route_table" "rt" {                     #route table  
+#   vpc_id = aws_vpc.main.id     
 
-  route {
-    cidr_block = "10.0.1.0/24"
-    gateway_id = aws_internet_gateway.igw.id
-  }
+#   route {
+#     cidr_block = "10.0.0.0/24"
+#     gateway_id = aws_internet_gateway.igw.id
+#   }
 
-  tags = {
-    Name = "internet_gateway"
-  }
-}
-
-
-resource "aws_route_table_association" "a" {          #subnet association for subnet 1  to route table 
-  subnet_id      = aws_subnet.sub-1.id
-  route_table_id = aws_route_table.rt.id   
-}
-
-resource "aws_route_table_association" "b" {         #subnet association for subnet 2  to route table 
-  subnet_id      = aws_subnet.sub-2.id
-  route_table_id = aws_route_table.rt.id  
-}
+#   tags = {
+#     Name = "internet_gateway"
+#   }
+# }
 
 
+# resource "aws_route_table_association" "a" {          #subnet association for subnet 1  to route table 
+#   subnet_id      = aws_subnet.sub-1.id
+#   route_table_id = aws_route_table.rt.id   
+# }
+
+# resource "aws_route_table_association" "b" {         #subnet association for subnet 2  to route table 
+#   subnet_id      = aws_subnet.sub-2.id
+#   route_table_id = aws_route_table.rt.id  
+# }
